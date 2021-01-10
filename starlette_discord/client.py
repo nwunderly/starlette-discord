26a624cf8c615945e3edd0bec4a2d1676f60855d
nwunderly <48489521+nwunderly@users.noreply.github.com>
2021-01-09 21:01:45 -0500

    more setup

---
+++ starlette_discord/client.py
@@ -0,0 +1,9 @@
+import aiohttp
+import oauthlib
+
+
+class DiscordOauthClient:
+    """Client for Discord Oauth2."""
+    def __init__(self):
+        pass
+
---
+++ starlette_discord/http.py
@@ -0,0 +1,9 @@
+import aiohttp
+import oauthlib
+
+
+class HTTP:
+    """HTTP Oauth2 client"""
+    def __init__(self):
+        pass
+
